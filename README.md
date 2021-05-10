1bb95c08a8f2a3e30320532ad39c5f196767a477
Aaron Den Hond <aaron.denhond@gmail.com>
2021-05-10 16:25:47 +0200

    first final version of cookie clicker JS functionalities : 3 buttons, tracking clicks and current cookies (points), added upgrades and scaling upgrade costs, updated README and comments in script.js file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # cookie-cutter
+
+BeCode Antwerp Giertz exercise making a cookie clicker.
+
+HTML,CSS and JS were used for this project.
+
+First step was adding the elements in HTML and the necessary JS to make the application interactive.
+
+HTML : 
+
+Made 2 labels for Cookies and Clicks tracking.
+Made 3 buttons, 1 for clicking and 2 for upgrades.
+
+JS : 
+
+Necessary variables, 2 upgradefunctions, eventlisteners on buttons.
+Tracking of clicks and current cookies, working upgrades on click and autoclick.
+
+ISSUES :
+
+Lost a lot of time trying to make a scalable upgradeCost function. Solution (thanks to classmate Tom and 
+coach Sicco : 
+const upgradeCost = (cost) => {
+
+    const modifier = 1.5; 
+    return cost * modifier;
+}
+
+in button.EventListener :
+cost = upgradeCost(cost);
+
+
--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-// button with counter on click initialized at 0
+// clicks initialized at 0 ,tracking clicks
+// selecting the label via clicksLabel to be able to change innerHTML easily
+
 let clicks = 0;
 let clicksLabel = document.getElementById("counter");
+
+// cookies initialized at 0, tracking cookies
+// selecting the label via cookiesLabel to be able to change innerHTML easily
 
 let cookies = 0;
 let cookiesLabel = document.getElementById("cookies");
 
+// create variable clickerModifier that acts as upgrade : higher clickerModifier means more cookies per click
 let clickerModifier = 0
 
+// button to register clicks, not really necessary
 let button = document.getElementById("run");
+
 
 let multiplierElement = document.getElementById("multiplier");
 
 let autoclickElement = document.getElementById("auto-click");
-let cost = 20
 
-document.getElementById("run").addEventListener("click", () => {
+//initial click upgrade cost and modifier
+let cost = 20
+const modifier = 1.5;
+
+//initial auto click upgrade cost, uses same modifier for now
+let autoCost = 50;
+
+// EventListeners on buttons to get the functionalities we want on click
+
+button.addEventListener("click", () => {
     clicks += 1; // clicks = clicks + 1
     clicksLabel.innerHTML = "Clicks:" + " " + clicks;
 
     cookies += 1 + clickerModifier; // niet clicks bij tellen
-    console.log(cookies);
+    
     cookiesLabel.innerHTML = "Cookies:" + " " + cookies;
 
 });
 
+
 document.getElementById("multiplier").addEventListener("click", () => {
-
-
 
     if (cookies < cost) {
         alert("Not enough credits!");
@@ -35,9 +50,11 @@ document.getElementById("multiplier").addEventListener("click", () => {
 
     else {
         clickerModifier = clickerModifier + 1; // moet zo om op te slagen clickerModifier += 1;
-        cookies = cookies - cost;
-        multiplierElement.innerHTML = `x${clickerModifier}`;
-        cost = upgradeCost(cost); 
+        cookies = Math.floor(cookies - cost);
+        let costRounded = Math.floor(cost * modifier);
+        multiplierElement.innerHTML = `x${clickerModifier}/ cookies ${costRounded}`;
+        cost = upgradeCost(cost);
+
 
     }
 });
@@ -45,20 +62,34 @@ document.getElementById("multiplier").addEventListener("click", () => {
 
 
 document.getElementById("auto-click").addEventListener("click", () => {
-    setInterval(function () {
-        button.click()
-    }, 1000)
+
+    if (cookies < autoCost) {
+        alert("Not enough credits!");
+    }
+    else {
+        setInterval(function () {
+            button.click()
+        }, 1000)
+        cookies = Math.floor(cookies - autoCost);
+        let autoCostRounded = Math.floor(autoCost * modifier);
+        autoclickElement.innerHTML = `x${clickerModifier}/ cookies ${autoCostRounded}`;
+        autoCost = upgradeAutoCost(autoCost);
+    }
 });
 
 
 
-// upgradecost scaling function
+// upgradecost scaling function for clickerModifier (aka clicker upgrade) and auto clicker upgrade
 
 const upgradeCost = (cost) => {
-    
+
     const modifier = 1.5; // modifier const always 1.5
     return cost * modifier;
-    
+}
 
+const upgradeAutoCost = (autoCost) => {
+
+    const modifier = 1.5; // modifier const always 1.5
+    return autoCost * modifier;
 }
 
