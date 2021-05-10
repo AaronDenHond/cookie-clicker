f2649c9412ba7c3e81fb5e490e04598ca975ab60
Aaron Den Hond <aaron.denhond@gmail.com>
2021-05-10 12:26:42 +0200

    working cookies/clicks and modifier

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let count = 0;
+// button with counter on click initialized at 0
+let clicks = 0;
+let clicksLabel = document.getElementById("counter"); 
+
+let cookies = 0;
+let cookiesLabel = document.getElementById("cookies");
+
+let clickerModifier = 0
+
 let button = document.getElementById("run");
-let counter = document.getElementById("counter");
 
 document.getElementById("run").addEventListener("click", () => {
-count += 1;
-counter.innerHTML =  count;
+clicks += 1; // clicks = clicks + 1
+clicksLabel.innerHTML = "Clicks:" + " " +  clicks;
+
+cookies += 1 + clickerModifier; // niet clicks bij tellen
+console.log(cookies);
+cookiesLabel.innerHTML = "Cookies:" + " " + cookies  ;
+
 });
+
+document.getElementById("multiplier").addEventListener("click", () => {
+
+ clickerModifier = clickerModifier + 1; // moet zo om op te slagen clickerModifier += 1;
+ 
+});
+
